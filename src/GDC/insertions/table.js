9c1f49b9e1bb8ff4865f85dd606ab632ffe6a0b4
Jason Brown <iam@browniefed.com>
2014-01-05 18:17:43 -0800

    Commenting ideas on how to make table work and resize with content

--- src/GDC/insertions/table.js
+++ src/GDC/insertions/table.js
@@ -23,3 +23,4 @@ define([], function() {
 //create an external table widget
 //It's actually a table, but each cell is a GDC
 //Then we can just fire events on it, maybe make a GDC inline/barebones
+//This will be passed a height, and if that height changes then it'll resize itself, fire heightchanged event and then the master GDC will take care of resizing the table cell
