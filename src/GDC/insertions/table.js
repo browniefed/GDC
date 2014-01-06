7496e3e2514fc7f0b7c170cea59b1ee23ad95d17
Jason Brown <iam@browniefed.com>
2014-01-05 21:39:25 -0800

    Just a rebuild

--- src/GDC/insertions/table.js
+++ src/GDC/insertions/table.js
@@ -31,8 +31,10 @@ define(['GDC/utils/tableFuncs'], function(tableUtils) {
 	insert.insertWidget = function() {
 		codeMirrorInstance = this._codemirror.getCodemirror();
 		codeMirrorConstructor = this._codemirror.codemirror;
-		var currentLine = codeMirrorInstance.doc.getCursor(true).line,
+		var currentLine = codeMirrorInstance.doc.getCursor(true),
+			endLine = codeMirrorInstance.doc.getCursor(false),
 			tableInstance = new Table(3,2);
+
 
 			tableInstance.setWidget(codeMirrorInstance.addLineWidget(currentLine, tableInstance.getDOM()));
 			tableInstance.initTableCells();
