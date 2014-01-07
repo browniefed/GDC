9f412e922f797e4c79d8ee03eb333683509c3b25
Jason Brown <iam@browniefed.com>
2014-01-07 07:24:17 -0800

    Busting out the table into its own widget

--- src/GDC/insertions/table.js
+++ src/GDC/insertions/table.js
@@ -1,4 +1,4 @@
-define(['GDC/utils/tableFuncs'], function(tableUtils) {
+define(['GDC/insertions/widgets/table'], function(Table) {
 
 	"use strict";
 
@@ -8,35 +8,7 @@ define(['GDC/utils/tableFuncs'], function(tableUtils) {
 
 	insert.name = 'table';
 
-	function Table(cellsX, cellsY) {
-		this.DOMEle = tableUtils.tableCreate(cellsX, cellsY);
-		this.tableCells = [];
-	};
 
-	Table.prototype.getDOM = function() {
-		return this.DOMEle;
-	};
-	Table.prototype.setWidget = function(widget) {
-		this.widget = widget;
-	}
-	Table.prototype.initTableCells = function() {
-		var tds = this.widget.node.querySelectorAll('td'),
-			tdCMs = [];
-		_(tds).forEach(function(td, index) {
-			tdCMs.push(new codeMirrorConstructor(td));
-		});
-		this.tableCells = tdCMs;
-	}
-
-	Table.prototype.destroy = function() {
-		this.widget.clear();
-	}
-
-	Table.prototype.addStyle = function(style) {
-		_.(this.tableCells).each(function(cell) {
-			cell.fire(style);
-		})
-	}
 
 	insert.insertWidget = function() {
 		codeMirrorInstance = this._codemirror.getCodemirror();
@@ -44,9 +16,8 @@ define(['GDC/utils/tableFuncs'], function(tableUtils) {
 		var currentLine = codeMirrorInstance.doc.getCursor(true).line,
 			tableInstance = new Table(2, 5);
 
-
 			tableInstance.setWidget(codeMirrorInstance.addLineWidget(currentLine, tableInstance.getDOM()));
-			tableInstance.initTableCells();
+			tableInstance.initTableCells(codeMirrorConstructor);
 			return tableInstance;
 	};
 
---
+++ src/GDC/insertions/widgets/table.js
@@ -0,0 +1,34 @@
+define(['GDC/utils/tableFuncs'], function(tableFuncs) {
+	function Table(cellsX, cellsY) {
+		this.DOMEle = tableUtils.tableCreate(cellsX, cellsY);
+		this.tableCells = [];
+	};
+
+	Table.prototype.getDOM = function() {
+		return this.DOMEle;
+	};
+	Table.prototype.setWidget = function(widget) {
+		this.widget = widget;
+	}
+	Table.prototype.initTableCells = function(codeMirrorConstructor) {
+		var tds = this.widget.node.querySelectorAll('td'),
+			tdCMs = [];
+		_(tds).forEach(function(td, index) {
+			tdCMs.push(new codeMirrorConstructor(td));
+		});
+		this.tableCells = tdCMs;
+	}
+
+	Table.prototype.destroy = function() {
+		this.widget.clear();
+	}
+
+	Table.prototype.addStyle = function(style) {
+		//For when we instantiate GDC instead of straight codemirror
+		_.(this.tableCells).each(function(cell) {
+			cell.fire(style);
+		})
+	}
+
+	return Table;
+})
